7e91fdb5a654d0bc772f0496fb1e711615db3563
Your Nameravindra6596 <you@example.comravipatil6596@gmail.com>
2021-05-30 23:21:03 +0530

    List UI Done

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Themes {
-  static ThemeData  lightTheme(BuildContext context) => ThemeData(
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
         appBarTheme: AppBarTheme(
@@ -14,7 +14,11 @@ class Themes {
           textTheme: Theme.of(context).textTheme,
         ),
       );
-  static ThemeData  darkTheme(BuildContext context) => ThemeData(
-       brightness: Brightness.dark,
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+        brightness: Brightness.dark,
       );
+
+  // Color
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
